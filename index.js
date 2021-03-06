a15ecfb782825a4f936c078a3e1366b0848e6398
Lavrentev Stanislav <hvoinii@gmail.com>
2021-03-06 19:17:16 +0600

    Send request when user stop type in input

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Hi there!');
+
+const fetchData = async (searchTerm) => {
+  const response = await axios.get('http://www.omdbapi.com/',{
+    params: {
+      apikey: 'b9a1a463',
+      s: searchTerm
+    }
+  });
+  console.log(response.data)
+}
+
+const input = document.querySelector('input');
+
+let timeoutId;
+
+const onInput = event => {
+  if(timeoutId) {
+    clearTimeout(timeoutId);
+  }
+  timeoutId = setTimeout(()=>{
+    fetchData(event.target.value);
+  }, 500);
+}
+
+input.addEventListener('input', onInput);
