d77bee7ddb133986313f01f124ab69f2c05b9fb4
Lavrentev Stanislav <hvoinii@gmail.com>
2021-03-10 20:04:58 +0600

    fix: results position

--- index.js
+++ index.js
@@ -14,34 +14,30 @@ const fetchData = async (searchTerm) => {
 };
 const root = document.querySelector("#root");
 root.innerHTML = `
-  <label><b>Search For A Movie</b></label>
-  <input type="text">
+  <label><b>Search For a Movie</b></label>
+  <input class="input" />
   <div class="dropdown">
     <div class="dropdown-menu">
-      <div class="dropdown-content results">
-      
-      </div>
+      <div class="dropdown-content results"></div>
     </div>
   </div>
 `;
 
 const input = document.querySelector("input");
 const dropdown = document.querySelector(".dropdown");
-const content = document.querySelector(".dropdown-content");
+const content = document.querySelector(".results");
 
 const onInput = async (event) => {
   const movies = await fetchData(event.target.value);
-
+  dropdown.classList.add("is-active");
   for (let movie of movies) {
     const link = document.createElement("a");
     link.classList.add("dropdown-item");
     link.innerHTML = `
         <img src="${movie.Poster}"/>
-        <p>${movie.Title}</p>
-      `;
+       ${movie.Title}`;
     content.appendChild(link);
   }
-  dropdown.classList.add("is-active");
 };
 
 input.addEventListener("input", debounce(onInput, 500));
